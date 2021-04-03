2173c038f204e33dff42f8415d2a5640e403c6b2
Andrew Nguyen Vo <awnguy01@louisville.edu>
2021-04-03 13:51:47 -0400

    Simplified table_validator

--- src/validators/table_validator.py
+++ src/validators/table_validator.py
@@ -7,17 +7,7 @@ class TableValidator(SQLiteParserVisitor):
 
     def visitFrom_clause(self, ctx: SQLiteParser.From_clauseContext):
         for table_or_subquery_ctx in ctx.table_or_subquery():
-            table_or_subquery = self.visitTable_or_subquery(
-                table_or_subquery_ctx)
-            if type(table_or_subquery) is str:
-                if find_table(table_or_subquery) is None:
+            if table_or_subquery_ctx.table_name():
+                if not find_table(table_or_subquery_ctx.table_name().getText()):
                     raise Exception(
                         'Source table not found in current working directory')
-
-        return super().visitFrom_clause(ctx)
-
-    def visitTable_or_subquery(self, ctx: SQLiteParser.Table_or_subqueryContext):
-        return self.visitTable_name(ctx.table_name())
-
-    def visitTable_name(self, ctx: SQLiteParser.Table_nameContext):
-        return ctx.getText()
